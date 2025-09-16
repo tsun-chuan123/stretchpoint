c4e6daa213833ddce8900989a57e5069b8431a1e
tsun-chuan123 <cges7901100@gmail.com>
2025-09-16 18:31:53 +0800

    Update llava_server packaging

--- ros2_ws/src/llava_server/setup.py
+++ ros2_ws/src/llava_server/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 package_name = 'llava_server'
 
 setup(
-    name=package_name,
+    name='llava-server',
     version='0.0.0',
     packages=find_packages(),
     data_files=[
